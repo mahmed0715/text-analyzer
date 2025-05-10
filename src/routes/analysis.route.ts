b365ba302b89913931b928110f8be220db2ffae7
Mustak Ahmed <mustak@liberate-labs.com>
2025-05-10 12:53:34 +0600

    Cleanup

--- src/routes/analysis.route.ts
+++ src/routes/analysis.route.ts
@@ -1,12 +1,12 @@
-import { Router } from "express";
-import * as analysisController from "../controllers/analysis.controller";
+import { Router } from 'express';
+import * as analysisController from '../controllers/analysis.controller';
 
 const router = Router();
 
-router.post("/", analysisController.createTextAnalysis);
-router.get("/", analysisController.getAllAnalyses);
-router.get("/:id", analysisController.getTextAnalysis);
-router.patch("/:id", analysisController.updateTextAnalysis);
-router.delete("/:id", analysisController.deleteTextAnalysis);
+router.post('/', analysisController.createTextAnalysis);
+router.get('/', analysisController.getAllAnalyses);
+router.get('/:id', analysisController.getTextAnalysis);
+router.patch('/:id', analysisController.updateTextAnalysis);
+router.delete('/:id', analysisController.deleteTextAnalysis);
 
 export default router;
